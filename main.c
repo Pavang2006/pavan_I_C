39f1a49a41df6d07be171ab45e19aeb3555e6445
Pavan. G Pavan <gpavangnan1947@gmail.com>
2024-11-26 04:15:24 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) 
+{int num;
+ printf("Enter a number:");
+ scanf("%d",&num);
+ for(int i=1;i<=num;i++)
+   {
+     printf("The number is %d\n",i);
+     
+   }
+  return 0;
+}
