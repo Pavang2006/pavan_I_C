846769bd225f4496a706f62690129eec58d5707d
Pavan. G Pavan <gpavangnan1947@gmail.com>
2024-11-26 02:26:41 -0500

    Add files via upload

---
+++ sumoftwonumbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,sum;
+  printf("ENTER THE NUMBERS");
+  scanf("%d%d",&a,&b);
+  sum=a+b;
+  printf("THE SUM OF THE NUMBERS IS %d\n",sum);
+  printf("Pavan G ECE");
+  return 0;
+}
