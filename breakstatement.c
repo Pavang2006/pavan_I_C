d45cac78c4c57cbea971b17e11a4b525f0472487
Pavan. G Pavan <gpavangnan1947@gmail.com>
2024-11-26 23:12:44 -0500

    Update breakstatement.c

--- breakstatement.c
+++ breakstatement.c
@@ -2,12 +2,14 @@
 
 int main(void) {
   for(int i=1;i<=10;i++)
-    {if(i==6)
     {
+      if(i==6){
       break;
-    }
+      }
+      
     printf("i=%d\n",i);
      }
+  
   printf("Pavan G ECE");
   return 0;
 }
