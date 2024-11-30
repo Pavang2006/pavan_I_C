c44e97cd5d76f45e0e65c0d91bf4b6cb8ebe94fd
Pavan. G Pavan <gpavangnan1947@gmail.com>
2024-11-30 11:39:35 +0530

    Update temperatureconversion.c

--- temperatureconversion.c
+++ temperatureconversion.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 int main(void){
-  float celsius,fahrenheit;
+  float celsius,kelvin,fahrenheit;
   printf("Enter temperature in celsius:");
   scanf("%f",&celsius);
+  kelvin=(273.15+ celsius);
   fahrenheit=(celsius*9/5)+32;
   printf("%.2fcelsius= %.2f  fahrenheit",celsius,fahrenheit);
+  printf("%.2f celsius= %.2f kelvin", celsius, kelvin) ;
   return 0;
 }
